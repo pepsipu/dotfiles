d5e276f3d5587003c425e8a335c1e72c81fa0a62
Sammy Hajhamid <sh@pepsi.pw>
2024-04-15 18:39:35 -0700

    update

--- configure.sh
+++ configure.sh
@@ -1,10 +1,3 @@
-# install nvim
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/bin/nvim
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# install python <-> vim
-pip3 install --user pynvim
